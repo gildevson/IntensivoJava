2e6d375deab75cf1e942273502166cce35abf02e
gildevson <gilsonfonseca92@gmail.com>
2024-11-13 23:58:55 -0300

    Adiiconando Primeira parte

---
+++ src/main/java/com/devsuperior/distlist/DistlistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.distlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/distlist/DistlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.distlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
